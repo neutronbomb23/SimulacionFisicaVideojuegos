7bc373556acaffb5af096aaeff334e4daa08d075
Dorjee <92365715+neutronbomb23@users.noreply.github.com>
2023-11-15 08:34:56 +0100

    Update VortexForceGenerator.cpp

--- skeleton/VortexForceGenerator.cpp
+++ skeleton/VortexForceGenerator.cpp
@@ -15,31 +15,31 @@ void VortexForceGenerator::updateForce(Particle* p, double t) {
 	p->addForce(vortexForce);
 }
 
-Vector3 VortexForceGenerator::calc(Vector3 c, Vector3 p) {
-	 //Reducir el valor de K para disminuir la velocidad del torbellino
-	const float K = 2.0f; 
-
-	 //Calcula la dirección tangencial del viento
-	Vector3 direction = Vector3(-(p.z - c.z), 50 - (p.y - c.y), p.x - c.x);
-
-	 //Ajusta la fuerza del viento en base a la distancia del centro
-	return K * direction;
-}
-
-////TORBELLINO ES ESPIRAL
 //Vector3 VortexForceGenerator::calc(Vector3 c, Vector3 p) {
-//    const float K = 50.0f; // Constante de fuerza del torbellino
+//	 //Reducir el valor de K para disminuir la velocidad del torbellino
+//	const float K = 2.0f; 
 //
-//  /*   Calcula la diferencia en posición y la distancia al centro del torbellino*/
-//    Vector3 diff = p - c;
-//    float distance = diff.magnitude();
+//	 //Calcula la dirección tangencial del viento
+//	Vector3 direction = Vector3(-(p.z - c.z), 50 - (p.y - c.y), p.x - c.x);
 //
-//     /*Evita la división por cero y aplica una fuerza decreciente con la distancia*/
-//    if (distance < 1.0f) {
-//        distance = 1.0f; // Establece un mínimo para evitar divisiones por cero
-//    }
-//    float strength = K / distance; // La fuerza disminuye con la distancia
-//
-//    /* Aplica la fuerza en una dirección tangencial*/
-//    return strength * Vector3(-(p.y - c.y), (p.z - c.z), -(p.x - c.x));
+//	 //Ajusta la fuerza del viento en base a la distancia del centro
+//	return K * direction;
 //}
+
+//TORBELLINO ES ESPIRAL
+Vector3 VortexForceGenerator::calc(Vector3 c, Vector3 p) {
+    const float K = 50.0f; // Constante de fuerza del torbellino
+
+  /*   Calcula la diferencia en posición y la distancia al centro del torbellino*/
+    Vector3 diff = p - c;
+    float distance = diff.magnitude();
+
+     /*Evita la división por cero y aplica una fuerza decreciente con la distancia*/
+    if (distance < 1.0f) {
+        distance = 1.0f; // Establece un mínimo para evitar divisiones por cero
+    }
+    float strength = K / distance; // La fuerza disminuye con la distancia
+
+    /* Aplica la fuerza en una dirección tangencial*/
+    return strength * Vector3(-(p.y - c.y), (p.z - c.z), -(p.x - c.x));
+}
